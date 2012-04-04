2ecf79ced1e88ec7881bc5d8eefcc877119aaab5
Mark Billie <mbillie1@gmail.com>
2012-04-03 20:50:26 -0500

    pe3 fixed

--- haskell/pe03.hs
+++ haskell/pe03.hs
@@ -1,11 +1,20 @@
 module Main where
 
-  prime :: Float -> Bool
+  prime :: Integral i => i -> Bool
   prime 2 = True
   prime x
-    | x <= 1                                                              = False
-    | (round x) `mod` 2 == 0                                              = False
-    | True `elem` [((round x) `mod` n == 0) | n <- map round [2..sqrt x]] = False
-    | otherwise                                                           = True
+    | x <= 1                                                                    = False
+    | x `mod` 2 == 0                                                            = False
+    | True `elem` [x `mod` n == 0 | n <- [2..truncate (sqrt (fromIntegral x))]] = False
+    | otherwise                                                                 = True
+
+  nextPrime :: Integer -> Integer 
+  nextPrime n = if prime (n + 1) then n + 1 else nextPrime (n + 1)
 
   primeNth x = head (drop (x - 1) (take (x) [n | n <- [1..], prime n]))
+
+  primeFactor :: Integer -> Integer -> Integer
+  primeFactor n x
+    | n == x             = x
+    | (x `mod` n == 0)   = primeFactor n (x `div` n)
+    | otherwise          = primeFactor (nextPrime n) x
