a95505a5cacfd83df2cc7429f72b4857b3db4b85
Mark Billie <mbillie1@gmail.com>
2012-04-18 23:41:29 -0500

    refactored

--- haskell/pe20.hs
+++ haskell/pe20.hs
@@ -1,8 +1,3 @@
 module Main where
 
-  factorial :: Integer -> Integer
-  factorial n
-    | n == 0         = 1
-    | otherwise = n * factorial (n - 1)
-
-  pe20 = sum [read [x] + 0 | x <- show(factorial 100)]
+  pe20 = sum [read [x] + 0 | x <- show $ product [x | x <- [1..100]]]
