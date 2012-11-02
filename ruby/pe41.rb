1331a2a2428c007bcf5a1dbdde5cc9ee9703cde1
Mark Billie <mbillie1@gmail.com>
2012-11-01 20:48:08 -0400

    pe41

---
+++ ruby/pe41.rb
@@ -0,0 +1,21 @@
+require 'prime'
+
+class Pe41
+  class << self
+    def max_1_to_n_pandigital_prime n
+      anagrams( (1..n).map(&:to_s) ).map(&:to_i).select(&:prime?).max || 0
+    end
+
+    def anagrams array
+      array.permutation.map(&:join)
+    end
+  end
+end
+
+time = Time.now
+max = 0
+1.upto(9) do |num|
+  attempt = Pe41.max_1_to_n_pandigital_prime(num)
+  max = attempt if attempt > max
+end
+puts "Solved: #{max} in #{Time.now - time} seconds"
---
+++ ruby/pe41_spec.rb
@@ -0,0 +1,11 @@
+require './pe41'
+
+describe Pe41 do
+  it "generates all anagrams of a string" do
+    Pe41.anagrams(%w[a b c]).should == %w(abc acb cba cab bac bca).sort
+  end
+
+  it "returns 4231 for 4" do
+    Pe41.max_1_to_n_pandigital_prime(4).should == 4231
+  end
+end
