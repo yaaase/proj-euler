8e681289456081d1b243629226e2057ea051817e
Mark Billie <mbillie1@gmail.com>
2012-11-10 01:53:41 -0500

    problem 17 technically correct but needs refactoring badly

--- ruby/pe17.rb
+++ ruby/pe17.rb
@@ -1,5 +1,5 @@
 class NumberWordHash
-  HASH = {
+  WORD_HASH = {
     1    => "one",
     2    => "two",
     3    => "three",
@@ -27,28 +27,54 @@ class NumberWordHash
     70   => "seventy",
     80   => "eighty",
     90   => "ninety",
-    100  => "hundred",
-    1000 => "thousand"
+    100  => "one hundred",
+    200  => "two hundred",
+    300  => "three hundred",
+    400  => "four hundred",
+    500  => "five hundred",
+    600  => "six hundred",
+    700  => "seven hundred",
+    800  => "eight hundred",
+    900  => "nine hundred",
+    1000 => "one thousand"
   }
 
   def self.[] x
-    if HASH.keys.include?(x)
-      HASH[x]
+    if WORD_HASH.keys.include?(x)
+      WORD_HASH[x]
     else
       build_word x
     end
   end
 
   def self.build_word x
-    digits, word = x.to_s.split(//).map(&:to_i), ""
-    place = 0
+    digits = x.to_s.split(//).map(&:to_i)
+    word, place, last_digit = "", 0, nil
     while digits.any? do
-      case place
-        when 0 then word = word + HASH[digits.pop]
-        when 1 then word = HASH[(digits.pop.to_s + "0").to_i] + " " + word
+      digit = digits.pop
+      if place == 0
+        word = word + (WORD_HASH[digit] || "")
+      elsif place == 1 && digit != 0 && digit != 1
+        word = WORD_HASH[(digit.to_s + "0").to_i] + " " + word
+      elsif place == 1 && digit == 1
+        word = WORD_HASH[(digit.to_s + last_digit.to_s).to_i]
+      elsif place == 2
+        word = WORD_HASH[digit] + " hundred and " + word
       end
+      last_digit = digit
       place += 1
     end
     word
   end
 end
+
+def problem_seventeen
+  [].tap do |word_list|
+    1.upto 1000 do |number|
+      word_list << NumberWordHash[number]
+    end
+  end.join("").gsub(/\s/,"").split(//).size
+end
+
+time = Time.now
+puts "Solved: #{problem_seventeen} in #{Time.now - time} seconds"
--- ruby/pe17_spec.rb
+++ ruby/pe17_spec.rb
@@ -3,5 +3,23 @@ require_relative './pe17'
 describe NumberWordHash do
   it { described_class[1].should == "one" }
 
+  it { described_class[40].should == "forty" }
+
   it { described_class[41].should == "forty one" }
+
+  it { described_class[99].should == "ninety nine" }
+
+  it { described_class[101].should == "one hundred and one" }
+
+  it { described_class[110].should == "one hundred and ten" }
+
+  it { described_class[111].should == "one hundred and eleven" }
+
+  it { described_class[200].should == "two hundred" }
+
+  it { described_class[234].should == "two hundred and thirty four" }
+
+  it { described_class[819].should == "eight hundred and nineteen" }
+
+  it { described_class[1000].should == "one thousand" }
 end
