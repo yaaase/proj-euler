983ec7fdf3097881e5acf289d29a32565b8adec4
Mark Billie <mbillie1@gmail.com>
2012-11-24 13:41:58 -0500

    problem fifty two

---
+++ ruby/pe52.rb
@@ -0,0 +1,29 @@
+class ProblemFiftyTwo
+  class << self
+
+    def same_digits? number, power
+      double = number * power
+      double.to_s.split(//).sort == number.to_s.split(//).sort
+    end
+
+    def loop_to_six? number
+      [6,5,4,3,2].each do |value|
+        return false unless same_digits? number, value
+      end
+      return true
+    end
+
+    def solve
+      start, solution = 1, nil
+      while !solution do
+        solution = start if loop_to_six? start
+        start += 1
+      end
+      solution
+    end
+
+  end
+end
+
+time = Time.now
+puts "Solved: #{ProblemFiftyTwo.solve} in #{Time.now - time} seconds."
---
+++ ruby/pe52_spec.rb
@@ -0,0 +1,9 @@
+require_relative './pe52'
+
+describe ProblemFiftyTwo do
+
+  it { described_class.same_digits?(125874, 2).should be_true }
+
+  it { described_class.same_digits?(12345, 2).should be_false }
+
+end
