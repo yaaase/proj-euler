df74e6191529452432064e3702a564da2283ae22
Mark Billie <mbillie1@gmail.com>
2013-01-18 17:18:36 -0500

    beer #2 and a lazy, slow, stupid solution to problem 24 that makes me more angry than happy

---
+++ ruby/pe24.rb
@@ -0,0 +1,10 @@
+class Permutator
+
+  def permutations *numbers
+    numbers.permutation.map(&:join).sort
+  end
+
+end
+
+time = Time.now
+puts "Solved: #{Permutator.new.permutations(0,1,2,3,4,5,6,7,8,9)[1_000_000]} in #{Time.now - time} seconds."
---
+++ ruby/pe24_spec.rb
@@ -0,0 +1,17 @@
+require_relative './pe24'
+
+describe Permutator do
+
+  let(:p) { Permutator.new }
+
+  it "generates lexical permutations" do
+    p.permutations(0,1,2).should ==
+      %w(012 021 102 120 201 210)
+  end
+
+  it "finds the damn solution" do
+    p.permutations(0,1,2,3,4,5,6,7,8,9)[1_000_000].should ==
+      "2783915460"
+  end
+
+end
